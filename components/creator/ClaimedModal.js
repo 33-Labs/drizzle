10dc83222c9d727f47efe19b623ff09882ed4fd8
lanford <lanfordcai@outlook.com>
2022-07-23 18:58:32 +0800

    feat: update ClaimModal

--- components/creator/ClaimedModal.js
+++ components/creator/ClaimedModal.js
@@ -32,7 +32,7 @@ export default function ClaimedModal(props) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative bg-white rounded-2xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm sm:w-full sm:p-6">
+              <Dialog.Panel className="relative bg-white rounded-2xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-sm w-full sm:p-6">
                 <div>
                   <label className="block w-full h-[80px] text-center text-[60px]">🎉</label>
                   <div className="mt-3 text-center sm:mt-5">
